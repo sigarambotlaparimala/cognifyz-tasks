ea2bc880e66e6a0090d12138a80b02cdf29fe2fd
sigarambotlaparimala <158692560+sigarambotlaparimala@users.noreply.github.com>
2026-02-25 17:35:00 +0530

    Delete Level-1/Task1.py

--- Level-1/Task1.py
+++
@@ -1,8 +0,0 @@
-def reverse_string(text):
-    reversed_text = ""
-    for char in text:
-        reversed_text = char + reversed_text
-    return reversed_text
-
-result = reverse_string("hello")
-print(result)
